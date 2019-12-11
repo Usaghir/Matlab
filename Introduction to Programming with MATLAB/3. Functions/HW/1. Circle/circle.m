97235063654b321522026307a984f648e99d181f
usaghir <umer.saghir@live.com>
2019-12-11 12:54:24 +0100

    Made some chagnes in the comments of 3.Functions HW problems

--- Introduction to Programming with MATLAB/3. Functions/HW/1. Circle/circle.m
+++ Introduction to Programming with MATLAB/3. Functions/HW/1. Circle/circle.m
@@ -1,7 +1,7 @@
 %{
-	1. Write a function called circle that takes a scalar input r. It needs to return an output called area that is the area of a circle 
-with radius r and a second output, cf that is the circumference of the same circle. You are allowed to use the built-in function pi. In fact, 
-you need to use it to get the value of π as accurately as possible.
+	1. Write a function called circle that takes a scalar input r. It needs to return an output called area that is the area of 
+a circle with radius r and a second output, cf that is the circumference of the same circle. You are allowed to use the built-in 
+function pi. In fact, you need to use it to get the value of π as accurately as possible.
 
 %}
 
--- Introduction to Programming with MATLAB/3. Functions/HW/4. Top Right/top_right.m
+++ Introduction to Programming with MATLAB/3. Functions/HW/4. Top Right/top_right.m
@@ -1,5 +1,7 @@
 %{
-	4. Write a function called top_right that takes two inputs: a matrix N and a scalar non-negative integer n, in that order, where each dimension of N is greater than or equal to n. The function returns the n-by-n square subarray of N located at the top right corner of N.
+	4. Write a function called top_right that takes two inputs: a matrix N and a scalar non-negative integer n, in that order, 
+where each dimension of N is greater than or equal to n. The function returns the n-by-n square subarray of N located at the top 
+right corner of N.
 
 %}
 
